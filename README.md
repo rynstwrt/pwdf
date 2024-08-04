80e39d0ae5898f2b1856a0be0cdd38f6355af8a7
Ryan Stewart <ryanstewartalex@gmail.com>
2024-08-04 18:14:00 -0500

    Init

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # pwdf
- A terminal command (Python module) to get the path of a file (pwd for files instead of directories).
+A terminal command (Python module) to get the path of a file (pwd for files instead of directories).
---
+++ src/pwdf/__main__.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+
+
+USAGE_MESSAGE = "Usage: pwdf <file>"
+
+
+def run():
+    if len(argv) != 2:
+        print(USAGE_MESSAGE)
+        return
+
+    file_path = argv[1]
+    if not path.isfile(file_path):
+        print("ERROR: The file specified could not be found.")
+        print(USAGE_MESSAGE)
+        return
+
+    abs_path = path.abspath(file_path)
+    print(abs_path)
+
+
+if __name__ == "__main__":
+    from sys import argv
+    from os import path
+
+    run()
